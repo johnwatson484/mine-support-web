153603bed3f0c52d04d9e8d7392ed97ff7d4e344
John Watson <johnwatson484@hotmail.com>
2019-09-28 22:59:31 +0100

    Probes added

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -1,5 +1,7 @@
 const routes = [].concat(
   require('../routes/home'),
+  require('../routes/healthy'),
+  require('../routes/healthz'),
   require('../routes/email'),
   require('../routes/confirmation'),
   require('../routes/public')
